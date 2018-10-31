f59b7b895f60e42e8d8991dd1e04381ac08ac68e
Josue Martinez <sid.jmartinez@gmail.com>
2018-10-31 09:21:37 -0600

    Primer Commit
    se agregaron las 3 graficas ojiva, frecuencia e histograma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,94 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  dataSource = {
+    "chart": {
+       "caption": "Expense Analysis",
+       "subCaption": "ACME Inc.",
+       "xAxisname": "Region",
+       "yAxisName": "Amount (In USD)",
+       "numberPrefix": "$",
+       "exportenabled": "1",
+       "theme": "fusion"
+    },
+    "categories": [{
+       "category": [{
+          "label": "East"
+       }, {
+          "label": "West"
+       }, {
+          "label": "South"
+       }, {
+          "label": "North"
+       }]
+    }],
+    "dataset": [{
+       "seriesName": "Actual Expenses",
+       "data": [{
+          "value": "1441290"
+       }, {
+          "value": "855912"
+       }, {
+          "value": "911404"
+       }, {
+          "value": "648136"
+       }]
+    }, {
+       "seriesName": "Budgeted Expenses",
+       "renderAs": "line",
+       "data": [{
+          "value": "1297430"
+       }, {
+          "value": "776485"
+       }, {
+          "value": "685352"
+       }, {
+          "value": "726791"
+       }]
+    }]
+ };
+  
+  constructor(){  
+    
+  }
+
+
+  
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  // x
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+
+  // y
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'}
+  ];
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+  // lineChart
+  public lineChartData:Array<any> = [
+    [65, 59, 80, 81, 56, 55, 40]
+  ];
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartType:string = 'line';
+ 
+  public randomizeType():void {
+    this.lineChartType = this.lineChartType === 'line' ? 'bar' : 'line';
+  }
+
+  //mixed
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ChartsModule } from 'ng2-charts';
+
+// Import angular-fusioncharts
+import { FusionChartsModule } from 'angular-fusioncharts';
+
+// Import FusionCharts library and chart modules
+import * as Charts from 'fusioncharts/fusioncharts.charts';
+
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+import FusionCharts from 'fusioncharts/core';
+import Column2D from 'fusioncharts/viz/column2d';
+// Include Below Snippet
+FusionChartsModule.fcRoot(FusionCharts,Charts, FusionTheme, Column2D, MSCombi2D);
+// Load Charts module
+import MSCombi2D from 'fusioncharts/viz/mscombi2d';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +26,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
+    FusionChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
